ecbc9aca60f49a4ade9aa923b9c73b7411b70cc9
Shakiv Hussain <51313301+shakivhussain@users.noreply.github.com>
2023-06-01 13:13:55 +0530

    Navigating to SingleAccountScreen with arguments

--- app/src/main/java/com/example/compose/rally/RallyDestinations.kt
+++ app/src/main/java/com/example/compose/rally/RallyDestinations.kt
@@ -22,6 +22,8 @@ import androidx.compose.material.icons.filled.Money
 import androidx.compose.material.icons.filled.MoneyOff
 import androidx.compose.material.icons.filled.PieChart
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
 
 /**
  * Contract for information needed on every Rally navigation destination
@@ -58,8 +60,12 @@ object SingleAccount : RallyDestination {
     // part of the RallyTabRow selection
     override val icon = Icons.Filled.Money
     override val route = "single_account"
-//    override val screen: @Composable () -> Unit = { SingleAccountScreen() }
+
+    //    override val screen: @Composable () -> Unit = { SingleAccountScreen() }
     const val accountTypeArg = "account_type"
+    val arguments = listOf(navArgument(accountTypeArg) { type = NavType.StringType })
+    val routeWithArgs = "${route}/{${accountTypeArg}}"
+
 }
 
 // Screens to be displayed in the top RallyTabRow
